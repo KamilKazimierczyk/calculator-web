c10f5ee6dedb15cdfbee6d3742d0de437aa14731
Kamil Kazimierczyk <kazimierczykkamil@wp.pl>
2023-01-17 08:55:24 +0100

    +GUI testing

--- cypress/e2e/calculator_spec.cy.js
+++ cypress/e2e/calculator_spec.cy.js
@@ -1,13 +1,53 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Calculator", () => {
+  it("math solving flow", () => {
+    cy.intercept("POST", "/calculate").as("calculate");
+
     cy.visit("/");
     cy.get("button[datasign='2']").click();
     cy.get("button[datasign='+']").click();
     cy.get("button[datasign='6']").click();
     cy.get("button[datasign='=']").click();
 
-    setTimeout(() => {
-      cy.get(".screen").should("eq", "8");
-    }, 500);
+    cy.wait("@calculate");
+
+    cy.get(".screen").should("have.text", "8");
+  });
+
+  it("GUI testing", () => {
+    const signs = [
+      "v",
+      "^",
+      "(",
+      ")",
+      "1",
+      "2",
+      "3",
+      "/",
+      "4",
+      "5",
+      "6",
+      "*",
+      "7",
+      "8",
+      "9",
+      "-",
+      ".",
+      "0",
+      "+",
+    ];
+
+    cy.visit("/");
+
+    signs.forEach((sign) => {
+      cy.get(`button[datasign='${sign}']`).click();
+      cy.get(".screen").should("have.text", sign);
+      cy.get(`button[datasign='CE']`).click();
+      cy.get(".screen").invoke("text").should("be.empty");
+    });
+
+    cy.get(`button[datasign='2']`).click();
+    cy.get(".screen").should("have.text", "2");
+    cy.get(`button[datasign='C']`).click();
+    cy.get(".screen").invoke("text").should("be.empty");
   });
 });
